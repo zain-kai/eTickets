2cf201955c8f76da3ead52d9455bf07cbe6dad4f
shaik <shaik@LAPTOP-8OQHSV20>
2022-12-15 19:01:44 +0530

    Add project files.

---
+++ eTickets/Data/ViewModels/NewMovieDropdownsVM.cs
@@ -0,0 +1,19 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+
+namespace eTickets.Data.ViewModels
+{
+    public class NewMovieDropdownsVM
+    {
+        public NewMovieDropdownsVM()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+        }
+        public List<Actor> Actors{ get; set; }
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+
+    }
+}
